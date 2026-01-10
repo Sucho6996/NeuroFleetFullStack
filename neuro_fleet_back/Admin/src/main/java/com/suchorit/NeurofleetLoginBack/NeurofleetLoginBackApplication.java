63daf5921f915ff5a693eb0ba463116b150f9c64
Sucho6996 <sucharitsaha2002@gmail.com>
2026-01-10 16:23:40 +0530

    fix needed

--- neuro_fleet_back/Admin/src/main/java/com/suchorit/NeurofleetLoginBack/NeurofleetLoginBackApplication.java
+++ neuro_fleet_back/Admin/src/main/java/com/suchorit/NeurofleetLoginBack/NeurofleetLoginBackApplication.java
@@ -2,8 +2,10 @@ package com.suchorit.NeurofleetLoginBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NeurofleetLoginBackApplication {
 
 	public static void main(String[] args) {
